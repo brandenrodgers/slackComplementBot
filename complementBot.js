490d1c28f0cbd3d7ab71e65980fde30f00e5fbc0
Branden Rodgers <brodgers@nanigans.com>
2015-12-15 14:40:46 -0500

    added more compliments

--- complementBot.js
+++ complementBot.js
@@ -1,14 +1,29 @@
 
-// TODO: add more coplements, remove hard-coded name, make chance of response much lower
 
 var SLACK_NAME = "branden"; // the user that the bot will respond to
 var RESPONSE_CHANCE = 100; // % chance of a response from the bot
 
+// Add '@' symbols wherever you want to use the person's name (only 1 per sentence)
 var complements = [
-  "@, you are smart, and everyone secretly thinks you are super cool.",
+  "@, you are smart, and everyone here thinks you are super cool.",
   "I wish everyone could be more like you, @.",
-  "You have an unbelievably deserving life.",
-  "Your co-workers don't deserve someone as perfect as you, @",
+  "Your co-workers don't deserve someone as perfect as you, @.",
+  "You're so delightful, @. I bet you make babies smile.",
+  "@, you are a strong, independant man. Don't let anyone here tell you otherwise",
+  "On a scale from 1 to 10, @ is an 11.",
+  "@, You're even more beautiful on the inside than you are on the outside.",
+  "You are making a difference, @.",
+  "@, I bet you sweat glitter.",
+  "Colors seem brighter when you're around, @.",
+  "I bet @ could survive a Zombie apocalypse.",
+  "Isn't @ like a breath of fresh air?",
+  "I bet @ does crossword puzzles in ink.",
+  "How does @ keep being so funny and making everyone laugh?",
+  "Forget this guys, lets just talk about how great @ is instead.",
+  "Does anyone have a Band Aid? I scraped my knee falling for @.",
+  "I appreciate @ more than Santa appreciates chimney grease.",
+  "Is it hot in here? Or is it just @?",
+  "Have you been going to the gym @? You look good today."
   ];
 
 
@@ -35,6 +50,8 @@ module.exports = function (req, res, next) {
 // Note: the sentence can only have one '@' symbol
 var replaceName = function(name, sentence) {
   var before = '', after = '';
+  name = name.charAt(0).toUpperCase() + name.slice(1); // capitalize the name
+  
   for (var i = 0; i < sentence.length; i++) {
     if (sentence[i] === '@') {
       after = sentence.substring(i+1, sentence.length);

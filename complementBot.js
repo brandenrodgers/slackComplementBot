ba8bc4d3b7349f02baae8cd6e71810a6439e09cf
Branden Rodgers <brodgers@nanigans.com>
2015-12-15 11:45:53 -0500

    added complements

--- complementBot.js
+++ complementBot.js
@@ -1,8 +1,18 @@
 
+
+var complements = [
+  "Rishi, you are smart, and everyone secretly thinks you are super cool.",
+  "I wish everyone could be more like you, Rishi.",
+  "You have an unbelievably deserving life.",
+  "Your co-workers don't deserve someone as perfect as you, Rishi",
+  ];
+
+
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
+  var index = Math.floor(Math.random() * 4);
   var botPayload = {
-    text : 'Hello, ' + userName + '!'
+    text : complements[index]
   };
 
   // avoid infinite loop
